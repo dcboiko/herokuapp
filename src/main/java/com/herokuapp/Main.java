2cce64d7f54a61295233bfb04ed9a93afa2fc17f
Diego Boiko <diego.cavalcanteb@gmail.com>
2024-06-25 13:38:37 -0300

    herokuapp automation init

---
+++ src/main/java/com/herokuapp/Main.java
@@ -0,0 +1,18 @@
+package com.herokuapp;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+
+    static WebDriver driver;
+    static String url = "https://the-internet.herokuapp.com/";
+
+    // public static void main(String[] args) {
+    //     driver = new ChromeDriver();
+    //     driver.get(url);
+    //     driver.findElement(By.xpath("//a[contains(text(),'Add/Remove Elements')]")).click();
+    //     driver.findElement(By.xpath("//button[contains(text(),'Delete')]")).click();
+
+    // }
+}
